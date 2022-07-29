fbc3b44058f5d673e29d85fbc8de795017cec202
tarisadinda20 <tarisadinda20@gmail.com>
2022-07-30 01:37:30 +0700

    add alert and delete function

--- src/pages/User/UserTable/index.js
+++ src/pages/User/UserTable/index.js
@@ -8,6 +8,8 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import EditUser from '../EditUser';
 import PictDetail from '../PictDetail';
 import CreateUser from '../CreateUser';
+import Swal from 'sweetalert2';
+import { ToastContainer, toast } from 'react-toastify';
 
 const UserTable = () => {
     const [userData, setUserData] = React.useState([]);
@@ -36,6 +38,7 @@ const UserTable = () => {
         ],
         rows: [...userData.map((user, index) => 
             ({
+                key: index,
                 Name: user.firstName,
                 Picture: <img className='user-img' src={user.picture} onClick={() => setModalImg({id: user.id})} />,
                 Action:   <div className="">
@@ -47,7 +50,13 @@ const UserTable = () => {
                             >
                                 Edit
                             </Button>
-                            <Button variant="contained" size="small" color="error" startIcon={<DeleteIcon/>}>
+                            <Button 
+                                variant="contained" 
+                                size="small" 
+                                color="error" 
+                                startIcon={<DeleteIcon/>}
+                                onClick={() => deleteUser(user.id)}
+                            >
                                 Delete
                             </Button>
                         </div>,
@@ -72,7 +81,46 @@ const UserTable = () => {
         .catch(err => console.error(err));
     }, []);
 
+    const deleteUser = (id) => {
+        Swal.fire({
+            text: 'Are you sure want to delete this data?',
+            icon: 'warning',
+            showDenyButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            denyButtonText: 'No',
+            confirmButtonText: 'Yes'
+        })
+        .then((result) => {
+            if (result.isConfirmed) {
+                axios({
+                    method: 'delete',
+                    url: `https://dummyapi.io/data/v1/user/${id}`,
+                    headers: {
+                        'content-type': 'application/json',
+                        'app-id': '62996cb2689bf0731cb00285'
+                    }
+                })
+                .then((result) => {
+                    toast.success('Successfully delete data!', {
+                        position: "top-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                    });
+                    const newUserData = userData.filter((user) => user.id !== id)
+                    setUserData(newUserData)
+                })
+            }             
+        })
+        .catch(err => console.error(err))
+    }
+
     return (<>
+        <ToastContainer />
         <div className="container">
             <div className="d-flex justify-content-start">
                 <Button variant="contained" onClick={() => setModalCreate(true)}>Create User</Button>
